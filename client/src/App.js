156976d08ad6948aa9801b03f9cd440ac773bef5
HEVERHD <hever11.hdgd@gmail.com>
2021-09-28 03:26:37 -0500

    Instalacion npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>MI PRIMER PROYECTO REAL </h1>
+		</div>
+	);
 }
 
 export default App;
